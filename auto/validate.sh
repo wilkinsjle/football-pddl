c4a14172e562b005a3337b8e9dc79f3840afadf9
Wilkins Leong <wilkins.leong@rea-group.com>
2019-11-04 16:01:40 +1100

    build step for solving/compiling a plan fomr problem and domain files

--- auto/validate.sh
+++ auto/validate.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "step 1!"
+echo "Solve and validate problem and domain files"
+
+domain=$(cat gfootball_domain.pddl)
+problem=$(cat gfootball_problem.pddl)
+
+JSON_STRING=$( jq -n \
+                  --arg dm "$domain" \
+                  --arg pr "$problem" \
+                  '{domain: $dm, problem: $pr}' )
+
+curl --data "$JSON_STRING" --HEADER "Content-Type: application/json" POST solver.planning.domains/solve-and-validate
